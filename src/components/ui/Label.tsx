5c7a87f4758654f5515d36a745d18eb8dbf9c1c5
Rudra Patni <coder7439@gmail.com>
2026-07-20 01:29:21 +0530

    basic code cleanup

--- src/components/ui/Label.tsx
+++ src/components/ui/Label.tsx
@@ -1,14 +1,15 @@
 import * as React from "react";
 
-export interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {}
+export interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {
+  ref?: React.Ref<HTMLLabelElement>;
+}
 
-export const Label = React.forwardRef<HTMLLabelElement, LabelProps>(
-  ({ className, ...props }, ref) => (
+export function Label({ className, ref, ...props }: LabelProps) {
+  return (
     <label
       ref={ref}
       className={`text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-gray-700 ${className}`}
       {...props}
     />
-  )
-);
-Label.displayName = "Label";
+  );
+}
